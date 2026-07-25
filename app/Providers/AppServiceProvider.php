70c26f487e829631b2f673ba9cf926ef3ccd1f8a
Josue <josuepe03@hotmail.com>
2026-07-24 23:22:47 -0600

    fixed: clase telescope

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,9 +19,9 @@ class AppServiceProvider extends ServiceProvider
         $this->app->register(\App\Contexts\Viviendas\Providers\ViviendasServiceProvider::class);
         $this->app->register(\App\Contexts\Clientes\Providers\ClientesServiceProvider::class);
 
-        if ($this->app->environment('local')) {
+        if ($this->app->environment('local') && class_exists(\Laravel\Telescope\TelescopeServiceProvider::class)) {
             $this->app->register(\Laravel\Telescope\TelescopeServiceProvider::class);
-            $this->app->register(\App\Providers\TelescopeServiceProvider::class); 
+            $this->app->register(TelescopeServiceProvider::class);
         }
     }
 
